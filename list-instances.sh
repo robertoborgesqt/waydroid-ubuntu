1b9d85182190586b323953a24728a7f765d8f056
robertoborgesqt <rborges67@me.com>
2025-04-01 10:23:37 -0300

    adicionando lista de instancias

--- list-instances.sh
+++ list-instances.sh
@@ -1,2 +1,14 @@
-sudo lxc-ls --fancy
 
+
+INSTANCE_NAME="$1"
+
+# Verifica se o script recebeu o nome do container como argumento
+if [ -z "$INSTANCE_NAME" ]; then
+    read -p "Por favor, insira o nome da instancia: " INSTANCE_NAME 
+    if [ -z "$INSTANCE_NAME" ]; then
+        echo -e "${YELLOW}Erro: O nome do instancia não pode estar vazio.${NC}"
+        exit 1
+    fi
+fi
+
+lxc list --project "$INSTANCE_NAME" 
